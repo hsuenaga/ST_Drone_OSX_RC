ca23556f054716674d94945de9969b914b771b11
SUENAGA Hiroki <hiroki_suenaga@mac.com>
2021-07-08 13:16:35 +0900

    Initial Commit

---
+++ ST_Drone_OSX_RC/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ST_Drone_OSX_RC
+//
+//  Created by SUENAGA Hiroki on 2021/07/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ST_Drone_OSX_RC/ST_Drone_OSX_RCApp.swift
@@ -0,0 +1,17 @@
+//
+//  ST_Drone_OSX_RCApp.swift
+//  ST_Drone_OSX_RC
+//
+//  Created by SUENAGA Hiroki on 2021/07/08.
+//
+
+import SwiftUI
+
+@main
+struct ST_Drone_OSX_RCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
